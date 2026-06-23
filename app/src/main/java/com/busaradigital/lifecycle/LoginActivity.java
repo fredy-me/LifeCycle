707a08c58011b3c484e8736266b6327a7bc8d3d0
fredy-me <fredymelkizedek@gmail.com>
2026-06-23 08:43:54 +0300

    Just  a readable space

--- app/src/main/java/com/busaradigital/lifecycle/LoginActivity.java
+++ app/src/main/java/com/busaradigital/lifecycle/LoginActivity.java
@@ -14,6 +14,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
