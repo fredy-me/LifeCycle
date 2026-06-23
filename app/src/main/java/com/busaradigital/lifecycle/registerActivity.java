0069652c6234ff31c9e53626e450b8f8cfee90e5
fredy-me <fredymelkizedek@gmail.com>
2026-06-23 11:45:35 +0300

    Enable login button to open login form

--- app/src/main/java/com/busaradigital/lifecycle/registerActivity.java
+++ app/src/main/java/com/busaradigital/lifecycle/registerActivity.java
@@ -1,6 +1,10 @@
 package com.busaradigital.lifecycle;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class registerActivity extends AppCompatActivity {
 
+    Button btnLogin;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openRegister = new Intent(registerActivity.this, LoginActivity.class);
+                startActivity(openRegister);
+            }
         });
     }
 }
