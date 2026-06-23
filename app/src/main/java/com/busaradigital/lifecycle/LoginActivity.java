6a30128e02c09d5539e868512019a22f12be1c81
fredy-me <fredymelkizedek@gmail.com>
2026-06-23 14:35:36 +0300

    add toast in LoginActivity

--- app/src/main/java/com/busaradigital/lifecycle/LoginActivity.java
+++ app/src/main/java/com/busaradigital/lifecycle/LoginActivity.java
@@ -1,9 +1,13 @@
 package com.busaradigital.lifecycle;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_login);
 
+        showLifecycleMessage("OnStart");
         //open register activity
         btnRegister = findViewById(R.id.btnRegister);
 
@@ -44,5 +49,45 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        showLifecycleMessage("onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showLifecycleMessage("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        showLifecycleMessage("onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        showLifecycleMessage("onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        showLifecycleMessage("onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        showLifecycleMessage("onDestroy");
+    }
+
+    private void showLifecycleMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        Log.d(TAG, message);
     }
 }
